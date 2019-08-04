78587b1088b03c654f411b1573979d867b59bb31
roger <rogerpan95@gmail.com>
2019-08-04 19:53:07 +0100

    Completed 05_functions

--- 05_functions/05_01_tasks.py
+++ 05_functions/05_01_tasks.py
@@ -6,10 +6,30 @@ Write a script that completes the following tasks.
 
 # define a function that determines whether the number is divisible by 4 or 7 and returns a boolean
 
+def divisible_4_7(x):
+    if x % 4 == 0 or x % 7 == 0:
+        return True
+    else:
+        return False
+
 # define a function that determines whether a number is divisible by both 4 and 7 and returns a boolean
+
+def divisible_4_and_7(x):
+    if x % 4 == 0 and x % 7 == 0:
+        return True
+    else:
+        return False
 
 # take in a number from the user between 1 and 1,000,000,000
 
+number = int(input("Input a number between 1 and 1,000,000,000: "))
+
 # call your functions, passing in the user input as the arguments, and set their output equal to new variables 
 
+i = divisible_4_7(number)
+j = divisible_4_and_7(number)
+
 # print your new variables to display the results
+
+print(i)
+print(j)
--- 05_functions/05_02_rps.py
+++ 05_functions/05_02_rps.py
@@ -8,13 +8,25 @@ Code a game of rock paper scissors.
 # the function should take in a number and return the string representation of the hand
 def get_hand(hand):
     # 0 = scissor, 1 = rock, 2 = paper
+    if hand == 0:
+        return "scissor"
+    elif hand == 1:
+        return "rock"
+    elif hand ==2:
+        return "paper"
 
     # for example if the variable hand is 0, it should return the string "scissor"
-    pass
+print(get_hand(1))
 
 # function should take in two hands and return a string "You won!" or "You lost :(" or "You tied!"
 def determine_winner(computer, player):
-    pass
+    if (computer == 0 and player == 1) or (computer == 1 and player == 2) or (computer==2 and player == 0):
+        return "You won!"
+    elif computer == player:
+        return "You tied!"
+    else:
+        return "You won!"
+
 
 
 
@@ -23,15 +35,24 @@ Start here
 '''
 
 # take in a number 0-2 from the user that represents their hand
+player = int(input("Input a number between 0 and 2: "))
 
 # generate a random number 0-2 to use for the computer's hand
+import random
+computer = random.randint(0,2)
 
 # call the function get_hand to get the string representation of the user's hand
+player_hand = get_hand(player)
 
 # call the function get_hand to get the string representation of the computer's hand
+computer_hand = get_hand(computer)
 
 # call the function determine_winner to figure out the winner
+winner = determine_winner(computer,player)
 
 # print out the player hand and computer hand
+print(player_hand)
+print(computer_hand)
 
 # print out the winner
+print(winner)
