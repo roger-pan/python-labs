6b91945003c0bcef073c4376835bd7c7bff58f11
roger <rogerpan95@gmail.com>
2019-08-29 12:45:30 +0100

    Completed 13_aggregate_functions

--- 13_aggregate_functions/13_01_enumerate.py
+++ 13_aggregate_functions/13_01_enumerate.py
@@ -1,3 +1,13 @@
 '''
 Demonstrate the use of the .enumerate() function.
 '''
+
+locations = ["Indonesia", "Spain", "Thailand", "Mexico", "USA"]
+print(enumerate(locations))
+
+my_list = ["apple", "banana", "orange"]
+new_list = []
+for index, value in enumerate(my_list):
+    new_list.append(tuple([index,value]))
+
+print(new_list)
--- 13_aggregate_functions/13_02_sum.py
+++ 13_aggregate_functions/13_02_sum.py
@@ -2,3 +2,12 @@
 Write a simple aggregate function, sum(), that takes a list a returns the sum.
 
 '''
+
+def sum(list):
+    agg = 0
+    for each in list:
+        agg += each
+    return agg
+
+print(sum([0,2,3,4]))
+
