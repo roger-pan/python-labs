eb26dc96839e521d428b222f00fd2b876573d025
roger <rogerpan95@gmail.com>
2019-08-29 14:30:47 +0100

    Completed 14_list_comprehensions

--- 14_list_comprehensions/14_01_comprehension.py
+++ 14_list_comprehensions/14_01_comprehension.py
@@ -9,3 +9,9 @@ word = "CodingNomads"
 result_list = ['C', 'o', 'd', 'i', 'n', 'g', 'N', 'o', 'm', 'a', 'd', 's']
 
 '''
+
+word = "supercalifragilisticexpialidocious"
+result_list = [letter for letter in word]
+print(result_list)
+
+print([letter.lower() for letter in 'CodingNomads'])
--- 14_list_comprehensions/14_02_positive.py
+++ 14_list_comprehensions/14_02_positive.py
@@ -5,3 +5,4 @@ Using list comprehension, create a list "positive" from the list
 '''
 
 numbers = [5, -8, 3, 10, -19, -22, 44, 2, -1, 4, 42]
+print([number for number in numbers if number > 0])
